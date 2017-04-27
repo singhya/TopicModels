a43c62f1eaf306275439d78412f9cbe855364f25
Niranjan Patil <meniranjan2309@gmail.com>
2017-04-27 14:48:17 -0700

    svm_9336_op

--- Evaluation/Classification/SVM/SVM_9336/data_prep.py
+++ Evaluation/Classification/SVM/SVM_9336/data_prep.py
@@ -1,11 +1,25 @@
+import numpy as np
+
 dir_labels = "docs/category-labels"
 dir_theta_csv =""
 
 def prep(k,algo):
 
     dir_theta = "docs/" + algo + "_op/" + algo + "_theta_k_" + str(k)
+
+    file = open(dir_theta, 'rb')
+    table_theta = [row.strip().split(' ') for row in file]
+
+    dir_psi = "docs/" + algo + "_op/" + algo + "_psi_k_" + str(k)
+
+    file = open(dir_psi, 'rb')
+    table_psi = [row.strip().split(' ') for row in file]
+
+    theta_psi = np.dot(np.asarray(table_theta,dtype=float),np.asarray(table_psi,dtype=float))
+
+
     meta = ""
-    for i in range(1, k + 1):
+    for i in range(1, 2*k + 1):
         meta += 'F' + str(i) + ','
     meta += 'Label'
 
@@ -18,6 +32,7 @@ def prep(k,algo):
     f_lab = open(dir_labels, "r")
 
     with open(dir_theta) as f:
+        i = 0
         for line in f:
             label = f_lab.readline()
             line = line.strip()
@@ -27,7 +42,13 @@ def prep(k,algo):
                     f1.write(word + ",")
             else:
                 f1.write(line + ",")
+
+            for x in theta_psi[i]:
+                f1.write(str(x)+",")
+
             f1.write(label)
+            i += 1
+
 
     f_lab.close()
     f.close()
@@ -35,3 +56,14 @@ def prep(k,algo):
 
 
 
+list_ectm = [1,5,10,15,20,25,30]
+algo = "ECTM"
+
+for k in list_ectm:
+    prep(k,algo)
+
+list_corr = [1,5,10,15,20,25,30,35]
+algo = "CorrLDA2"
+
+for k in list_corr:
+    prep(k, algo)
