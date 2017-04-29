2cef98cae55a2378cee060b2962b0756f4d537c8
Niranjan Patil <meniranjan2309@gmail.com>
2017-04-28 22:41:27 -0700

    svm precision code

--- Evaluation/Classification/SVM/SVM_9336/svm.py
+++ Evaluation/Classification/SVM/SVM_9336/svm.py
@@ -2,8 +2,8 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
 from sklearn.cross_validation import train_test_split
 import pandas as pd
-from sklearn.metrics import precision_recall_curve
-from sklearn.metrics import average_precision_score
+import numpy as np
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
 def svm(file):
     Meta = pd.read_csv(file)
@@ -16,38 +16,38 @@ def svm(file):
     y_train = y_train.astype(float)
 
     svm1 = OneVsRestClassifier(LinearSVC(random_state=0)).fit(x_train, y_train)
+    acc = svm1.score(x_test, y_test)
+    y_pred = svm1.predict(x_test)
 
-    #pred = svm1.predict(x_test)
-    return svm1.score(x_test, y_test)
-    '''
-    y_score = svm1.fit(x_train, y_train).decision_function(x_test)
-    precision = []
-    recall = []
-    average_precision = []
-    for i in range(9):
-        precision[i], recall[i], _ = precision_recall_curve(y_test[:, i],
-                                                            y_score[:, i])
-        average_precision[i] = average_precision_score(y_test[:, i], y_score[:, i])
-    '''
+    y_test = np.array(y_test)
+    y_pred = np.array(y_pred)
 
+    return acc,f1_score(y_test, y_pred,average=None),precision_score(y_test, y_pred,average=None),recall_score(y_test, y_pred,average=None)
 
 list_ectm = [1,5,10,15,20,25,30]
 algo = "ECTM"
 
 for k in list_ectm:
-    sc = svm("docs/" + algo + "_op/csv/" + algo + "_theta_k_" + str(k) + ".csv")
+    sc,f1,prec,rec = svm("docs/" + algo + "_op/csv/" + algo + "_theta_k_" + str(k) + ".csv")
     print algo + "_k_" + str(k) + ": "+str(sc)
+    for i in range(0,9):
+        print "Class:",i,"=>F-1: ",f1[i],"Precision: ",prec[i],"Recall: ",rec[i]
 
 list_corr = [1,5,10,15,20,25,30,35]
 algo = "CorrLDA2"
 
 for k in list_corr:
-    sc = svm("docs/" + algo + "_op/csv/" + algo + "_theta_k_" + str(k) + ".csv")
-    print algo + "_k_" + str(k) + ": "+str(sc)
+    sc,f1,prec,rec = svm("docs/" + algo + "_op/csv/" + algo + "_theta_k_" + str(k) + ".csv")
+    print algo + "_k_" + str(k) + ": " + str(sc)
+    for i in range(0, 9):
+        print "Class:",i,"=>F-1: ", f1[i], "Precision: ", prec[i], "Recall: ", rec[i]
+
 
 list_lda = [1,5,10,15,20,25,30,35]
 algo = "LDA"
 
 for k in list_corr:
-    sc = svm("docs/" + algo + "_op/csv/" + algo + "_theta_k_" + str(k) + ".csv")
-    print algo + "_k_" + str(k) + ": "+str(sc)
+    sc,f1,prec,rec = svm("docs/" + algo + "_op/csv/" + algo + "_theta_k_" + str(k) + ".csv")
+    print algo + "_k_" + str(k) + ": " + str(sc)
+    for i in range(0, 9):
+        print "Class:",i,"=> F-1: ", f1[i], "Precision: ", prec[i], "Recall: ", rec[i]
